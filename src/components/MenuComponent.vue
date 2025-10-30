1f85d0b33682f2a7b4cbca3722daf12cd5b2d9ba
marcospedroche05 <marcospedroche62@gmail.com>
2025-10-30 10:03:59 +0100

    ...

--- src/components/MenuComponent.vue
+++ src/components/MenuComponent.vue
@@ -4,7 +4,7 @@
       <li><router-link to="/">Deportes</router-link></li>
       <li><router-link to="/comics">Comics</router-link></li>
       <li><router-link to="/numeros">Numeros</router-link></li>
-      <li><router-link to="/doble">Doble sin</router-link></li>
+      <li><router-link to="/doble">Doble sin alcohol</router-link></li>
       <li><router-link to="/doble/3">Doble de 3</router-link></li>
     </ul>
   </div>
